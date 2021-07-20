844ab83a3adb0a378cbe58265e80f26efe246588
Mustafa Durmaz <mdmz_001@hotmail.com>
2021-07-20 20:47:03 +0300

    Add files via upload

---
+++ projects/css/component_recipe/script.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".component-recipe-6 .next-btn",
+        prevEl: ".component-recipe-6 .prev-btn",
+      }
+  });
